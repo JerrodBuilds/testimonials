0b1c2586220868ace855a0d4018e399ad51c6429
j <j@js-iMac.local>
2025-10-17 15:56:51 -0700

    application

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask #Brings the Flask class from the flask module allowing us to use it in our code
 
 app = Flask(__name__) #Creates a new flask object and assigns it to a variable called app
-
+application = app
 @app.route('/')
 def index():
     return 'hello'
